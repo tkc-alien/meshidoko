fb0f6c26fe3b179a2bebbd073be62c6cecc80f30
kouhei-takashima <kouhei-takashima@dip-net.co.jp>
2024-04-13 17:29:22 +0900

    add: iOS用のGoogleMap設定を追加

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -1,13 +1,27 @@
 import UIKit
 import Flutter
+import GoogleMaps
 
 @UIApplicationMain
 @objc class AppDelegate: FlutterAppDelegate {
-  override func application(
-    _ application: UIApplication,
-    didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
-  ) -> Bool {
-    GeneratedPluginRegistrant.register(with: self)
-    return super.application(application, didFinishLaunchingWithOptions: launchOptions)
-  }
+    override func application(
+        _ application: UIApplication,
+        didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
+    ) -> Bool {
+        setupGoogleMaps()
+        GeneratedPluginRegistrant.register(with: self)
+        return super.application(application, didFinishLaunchingWithOptions: launchOptions)
+    }
+    
+    /// GoogleMaps初期化
+    private func setupGoogleMaps() {
+        // GoogleMapsApiKeyをplistから取得する
+        let apiKey = Bundle.main.object(forInfoDictionaryKey: "GoogleMapsApiKey") as? String
+        // APIKeyが取得できればGoogleMapsServiceに渡す
+        if let apiKey, !apiKey.isEmpty {
+            GMSServices.provideAPIKey(apiKey)
+        } else {
+            fatalError("GoogleMapsApiKeyが取得できません。")
+        }
+    }
 }
